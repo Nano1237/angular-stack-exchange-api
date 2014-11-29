b97532ab510a604b03d252650c27f8788366c1ec
Tim <Tim@192.168.178.23>
2014-11-29 01:08:42 +0100

    changed the name of the Api Object from SE_api to SE

--- angular-stack-exchange-api.js
+++ angular-stack-exchange-api.js
@@ -1,3 +1,46 @@
 //https://api.stackexchange.com/2.2/users/2217462?order=desc&sort=reputation&site=stackoverflow
 
 //https://api.stackexchange.com/docs
+
+angular.module('angular-stack-exchange-api', [])
+
+        .constant('angular-stack-exchange-api.apiUrl', 'https://api.stackexchange.com/2.2/')
+
+
+        .factory('angular-stack-exchange-api.objectToUrl', function() {
+            return function(object) {
+                var ret = '';
+                for (var i in object) {
+                    ret += i + '=' + object[i]+'&';
+                }
+                return encodeURI(ret.trim('&'));
+            };
+        })
+
+        .factory('angular-stack-exchange-api.api', [
+            '$q',
+            '$http',
+            'angular-stack-exchange-api.apiUrl',
+            'angular-stack-exchange-api.objectToUrl',
+            function($q, $http, url, ourl) {
+                return function(api_target, apiData) {
+                    var deferred = $q.defer();
+                    var dataAppend = '';
+                    if (typeof apiData !== 'undefined') {
+                        dataAppend = '?' + ourl(apiData);
+                    }
+                    $http.get(url + api_target + dataAppend).success(deferred.resolve);
+                    return deferred.promise;
+                };
+            }
+        ])
+
+
+        .factory('SE', [
+            'angular-stack-exchange-api.api',
+            function(api) {
+                return {
+                    api: api
+                };
+            }
+        ]);
