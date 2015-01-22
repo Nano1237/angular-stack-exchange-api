44a800a610b5b7245cf57b70531cb696ff516a9a
Tim <tim.ruecker@ymail.com>
2015-01-22 12:53:12 +0100

    added documentation

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-angular-stack-exchange-api
-==========================
+#angular-stack-exchange-api
+
+Lets you make easy ajax calls to the stackexchange api
+
+###Usage
+
+- Set the `angular-stack-exchange-api` as dependency
+```javascript
+angular.module('myApp', [
+    'angular-stack-exchange-api'
+]);
+```
+- Include the `SE` factory
+```javascript
+myApp.controller('ExampleCtrl',[
+    'SE',
+    function(SE) {
+       //your code goes here
+    }
+]);
+```
+- Make any call you like
+```javascript
+SE.api('CALLDATA').then(function(a){
+    console.log('response: ',a);
+});
+```
+
+How the api exacly works can you read [here](https://api.stackexchange.com/docs).
--- angular-stack-exchange-api.js
+++ angular-stack-exchange-api.js
@@ -1,46 +1,75 @@
-//https://api.stackexchange.com/2.2/users/2217462?order=desc&sort=reputation&site=stackoverflow
+/**
+ * 
+ * angular-stack-exchange-api v0.1
+ * (c) 2015 Tim Rücker
+ * License: Apache License
+ */
+(function(angular) {
+    'use strict';
+    angular.module('angular-stack-exchange-api', [])
 
-//https://api.stackexchange.com/docs
+            /**
+             * 
+             * @description The Api Url of the Stackexchange api
+             * @returns {String}
+             */
+            .constant('angular-stack-exchange-api.apiUrl', 'https://api.stackexchange.com/2.2/')
 
-angular.module('angular-stack-exchange-api', [])
 
-        .constant('angular-stack-exchange-api.apiUrl', 'https://api.stackexchange.com/2.2/')
-
-
-        .factory('angular-stack-exchange-api.objectToUrl', function() {
-            return function(object) {
-                var ret = '';
-                for (var i in object) {
-                    ret += i + '=' + object[i]+'&';
-                }
-                return encodeURI(ret.trim('&'));
-            };
-        })
-
-        .factory('angular-stack-exchange-api.api', [
-            '$q',
-            '$http',
-            'angular-stack-exchange-api.apiUrl',
-            'angular-stack-exchange-api.objectToUrl',
-            function($q, $http, url, ourl) {
-                return function(api_target, apiData) {
-                    var deferred = $q.defer();
-                    var dataAppend = '';
-                    if (typeof apiData !== 'undefined') {
-                        dataAppend = '?' + ourl(apiData);
+            /**
+             * 
+             * @description Formats an Onedimensional Object to a Key-value pair url
+             * @returns {String}
+             */
+            .factory('angular-stack-exchange-api.objectToUrl', function() {
+                return function(object) {
+                    var ret = '';
+                    for (var i in object) {
+                        ret += i + '=' + object[i] + '&';
                     }
-                    $http.get(url + api_target + dataAppend).success(deferred.resolve);
-                    return deferred.promise;
+                    return encodeURI(ret.trim('&'));
                 };
-            }
-        ])
+            })
 
+            /**
+             * 
+             * @description Makes an ajax call to the stackexchange api with the required parameters
+             * @param {Object} $q
+             * @param {Object} $http
+             * @param {Object} apiUrl
+             * @param {Function} objectToUrl
+             * @returns {Object}
+             */
+            .factory('angular-stack-exchange-api.api', [
+                '$q',
+                '$http',
+                'angular-stack-exchange-api.apiUrl',
+                'angular-stack-exchange-api.objectToUrl',
+                function($q, $http, apiUrl, objectToUrl) {
+                    return function(api_target, apiData) {
+                        var deferred = $q.defer();
+                        var dataAppend = '';
+                        if (typeof apiData !== 'undefined') {
+                            dataAppend = '?' + objectToUrl(apiData);
+                        }
+                        $http.get(apiUrl + api_target + dataAppend).success(deferred.resolve);
+                        return deferred.promise;
+                    };
+                }
+            ])
 
-        .factory('SE', [
-            'angular-stack-exchange-api.api',
-            function(api) {
-                return {
-                    api: api
-                };
-            }
-        ]);
+            /**
+             * 
+             * @description Makes all the Apifucntions accessable
+             * @param {Object} api
+             * @returns {_L7._L66.Anonym$3}
+             */
+            .factory('SE', [
+                'angular-stack-exchange-api.api',
+                function(api) {
+                    return {
+                        api: api
+                    };
+                }
+            ]);
+})(angular);
